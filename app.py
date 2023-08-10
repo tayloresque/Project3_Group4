7fa39df21724743460ef5a0ce536536a8ac9ef87
Wook22 <1994wook@gmail.com>
2023-08-09 21:22:34 -0700

    code add

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
-from flask import Flask, jsonify
-from pymongo import MongoClient
+from flask import Flask, jsonify, render_template, redirect
 import json
+import pymongo
 
 app = Flask(__name__)
 
-# Define your MongoDB connection
-mongo_client = MongoClient('localhost', 27017)
-db = mongo_client['hotel_db']
-collection = db['hotels']
+# Connect to the MongoDB server
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+
+# Choose a database and collection
+db = client.mydatabase  # Replace 'mydatabase' with your preferred database name
+collection = db.mycollection  # Replace 'mycollection' with your preferred collection name
 
 # Load data from JSON file and insert into MongoDB
 def load_data_to_mongodb():
     with open('data/hotel_final.json', 'r') as json_file:
         data = json.load(json_file)
-        for document in data:
-            collection.insert_one(document)
+        collection.insert_many(data)
+
+# Retrieve data from MongoDB
+def get_hotel_data():
+    data = list(collection.find({}, {'_id': 0}))
+    return data
 
 @app.route('/get_hotel_data')
-def get_hotel_data():
-    # Retrieve data from MongoDB
-    data = list(collection.find({}, {'_id': 0}))
+def route_get_hotel_data():
+    data = get_hotel_data()
     return jsonify(data)
 
 if __name__ == '__main__':
@@ -29,5 +35,11 @@ if __name__ == '__main__':
 
     app.run(debug=True)
 
+    @app.route("/")
+    def home():
+        collection_list = db.collection.find_one()
+        return render_template('index.html', collections=collection_list)
 
-
+    @app.route("/tableee")
+    def table():
+        return render_template('hotel_table.html')
