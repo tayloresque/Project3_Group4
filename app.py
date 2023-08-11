c35e44bcd96833f9add6b6cde6f5d8e856c1a714
Stephanie Loomer <129898378+sdloomer@users.noreply.github.com>
2023-08-10 18:03:15 -0700

    Testing flask
    Testing flask import again

--- app.py
+++ app.py
@@ -1,41 +1,6 @@
-# from flask import Flask, jsonify
-# from pymongo import MongoClient
-# import json
-
-# app = Flask(__name__)
-
-# # Define your MongoDB connection
-# mongo_client = MongoClient('localhost', 27017)
-# db = mongo_client['hotel_db']
-# collection = db['hotels']
-
-# # Load data from JSON file and insert into MongoDB
-# def load_data_to_mongodb():
-#     with open('data/hotel_final.json', 'r') as json_file:
-#         data = json.load(json_file)
-#         for document in data:
-#             collection.insert_one(document)
-
-# @app.route('/get_hotel_data')
-# def get_hotel_data():
-#     # Retrieve data from MongoDB
-#     data = list(collection.find({}, {'_id': 0}))
-#     return jsonify(data)
-
-# if __name__ == '__main__':
-#     # Load data into MongoDB only if the collection is empty
-#     if collection.count_documents({}) == 0:
-#         load_data_to_mongodb()
-
-#     app.run(debug=True)
-
-
-
-
 # Import dependencies
 from flask import Flask, jsonify, render_template
-from pymongo import MongoClient
-# Import main file (.py) here
+import pymongo
 
 #################################################
 # Flask Setup
@@ -43,9 +8,10 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # Establish MongoDB connection
-mongo_client = MongoClient("localhost", 27017)
-db = mongo_client["hotel_db"]
-collection = db["hotels"]
+conn = "mongodf://localhost:27017"
+client = pymongo.MongoClient(conn)
+db = client.hotel_db
+collection = db.hotel
 
 #################################################
 # Flask Routes
